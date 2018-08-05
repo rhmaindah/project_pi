44ebc430946101f180647cefdb6288850bebee65
frmnjn <firmanbudis@gmail.com>
2018-08-05 18:16:20 +0700

    fix insert in contact.php

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -13,7 +13,12 @@ class Contact{
 	}
 
 	function insert(){
-		$query = "insert into " .$this->table_name." values('',?,?,?,?)";
+		$query = "insert into " .$this->table_name." values(?,?,?,?)";
+		//yang teliti ndah !!!
+		//ini kolomnya kan ada 4 di database
+		//terus kenapa ini ada 5????
+		//besok besok jangan panik, cek dulu satu satu, kemungkinan salahnya dimana. hmmmmm
+		//udah salahnya gitu doang ckck
 		$stmt = $this->conn->prepare($query);
 		$stmt->bindParam(1, $this->nl);
 		$stmt->bindParam(2, $this->em);
